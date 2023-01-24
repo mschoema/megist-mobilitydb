bda3179a5702a05b43f9c2200a56a8b8ceeaad2c
Maxime Schoemans <maxime.schoemans@ulb.ac.be>
2023-01-24 17:43:22 -0500

    Add index with MergeSplit as ExtractValue function

--- megist_mobilitydb--1.0.sql
+++ megist_mobilitydb--1.0.sql
@@ -9,24 +9,25 @@ CREATE FUNCTION tpoint_megist_compress(internal)
   RETURNS internal
   AS 'MODULE_PATHNAME', 'Tpoint_megist_compress'
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
-CREATE FUNCTION tpoint_megist_equisplit(internal, internal, internal)
-  RETURNS internal
-  AS 'MODULE_PATHNAME', 'Tpoint_megist_equisplit'
-  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 CREATE FUNCTION tpoint_megist_options(internal)
   RETURNS void
   AS 'MODULE_PATHNAME', 'Tpoint_megist_options'
   LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
+/******************************************************************************/
+
+/* Equisplit */
+
+CREATE FUNCTION tpoint_megist_equisplit(internal, internal, internal)
+  RETURNS internal
+  AS 'MODULE_PATHNAME', 'Tpoint_megist_equisplit'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
 CREATE OPERATOR CLASS megist_tpoint_equisplit_ops
-  DEFAULT FOR TYPE tgeompoint USING MEGIST AS
+  FOR TYPE tgeompoint USING MEGIST AS
   STORAGE stbox,
   -- overlaps
-  OPERATOR  3    && (tgeompoint, timestamptz),
-  OPERATOR  3    && (tgeompoint, tstzset),
   OPERATOR  3    && (tgeompoint, tstzspan),
-  OPERATOR  3    && (tgeompoint, tstzspanset),
-  OPERATOR  3    && (tgeompoint, geometry),
   OPERATOR  3    && (tgeompoint, stbox),
   OPERATOR  3    && (tgeompoint, tgeompoint),
   -- functions
@@ -40,3 +41,29 @@ CREATE OPERATOR CLASS megist_tpoint_equisplit_ops
   FUNCTION  12 tpoint_megist_equisplit(internal, internal, internal);
 
 /******************************************************************************/
+
+/* Mergesplit */
+
+CREATE FUNCTION tpoint_megist_mergesplit(internal, internal, internal)
+  RETURNS internal
+  AS 'MODULE_PATHNAME', 'Tpoint_megist_mergesplit'
+  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE OPERATOR CLASS megist_tpoint_mergesplit_ops
+  DEFAULT FOR TYPE tgeompoint USING MEGIST AS
+  STORAGE stbox,
+  -- overlaps
+  OPERATOR  3    && (tgeompoint, tstzspan),
+  OPERATOR  3    && (tgeompoint, stbox),
+  OPERATOR  3    && (tgeompoint, tgeompoint),
+  -- functions
+  FUNCTION  1  gist_tgeompoint_consistent(internal, tgeompoint, smallint, oid, internal),
+  FUNCTION  2  stbox_gist_union(internal, internal),
+  FUNCTION  3  tpoint_megist_compress(internal),
+  FUNCTION  5  stbox_gist_penalty(internal, internal, internal),
+  FUNCTION  6  stbox_gist_picksplit(internal, internal),
+  FUNCTION  7  stbox_gist_same(stbox, stbox, internal),
+  FUNCTION  10 tpoint_megist_options(internal),
+  FUNCTION  12 tpoint_megist_mergesplit(internal, internal, internal);
+
+  /******************************************************************************/
